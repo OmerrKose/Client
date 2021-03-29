195cfb9aa38b97a239b1786b04acdea140c9d6ee
omerkose <omerkose@sabanciuniv.edu>
2021-03-29 20:29:39 +0300

    Initial commit

---
+++ inLab1OmerKose25224/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace inLab1OmerKose25224
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIp = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxIp
+            // 
+            this.textBoxIp.Location = new System.Drawing.Point(105, 60);
+            this.textBoxIp.Name = "textBoxIp";
+            this.textBoxIp.Size = new System.Drawing.Size(143, 20);
+            this.textBoxIp.TabIndex = 0;
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(105, 102);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(143, 20);
+            this.textBoxPort.TabIndex = 1;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(105, 144);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(143, 20);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Port:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Name:";
+            // 
+            // richTextBoxLog
+            // 
+            this.richTextBoxLog.Location = new System.Drawing.Point(276, 60);
+            this.richTextBoxLog.Name = "richTextBoxLog";
+            this.richTextBoxLog.Size = new System.Drawing.Size(266, 259);
+            this.richTextBoxLog.TabIndex = 6;
+            this.richTextBoxLog.Text = "";
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(105, 187);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 7;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 350);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.richTextBoxLog);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIp;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBoxLog;
+        private System.Windows.Forms.Button buttonConnect;
+    }
+}
+
---
+++ inLab1OmerKose25224/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inLab1OmerKose25224
+{
+    public partial class Form1 : Form
+    {
+
+        bool connected = false;
+        bool terminating = false;
+        
+        Socket clientSocket;
+
+        private void form1FormClosing(object sender, EventArgs e)
+        {
+            connected = false;
+            terminating = false;
+            Environment.Exit(0);
+        }
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(form1FormClosing);
+            InitializeComponent();
+        }
+
+        private void Recieve()
+        {
+            while (connected)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64];
+                    clientSocket.Receive(buffer);
+
+                    int sum = 0;
+                    string incommingMessage = Encoding.Default.GetString(buffer);
+                    incommingMessage = incommingMessage.Substring(0, incommingMessage.IndexOf("\0"));
+                    richTextBoxLog.AppendText("Server: " + incommingMessage + "\n");
+
+
+                    int number = 0;
+                    Int32.TryParse(incommingMessage, out number);
+
+                    while (number != 0)
+                    {
+                        sum += number % 10;
+                        number /= 10;
+                    }
+
+                    
+
+                    string message = sum + " " + textBoxName.Text;
+                    richTextBoxLog.AppendText("Message sent: " + message + "\n");
+
+                    byte[] buffer2 = Encoding.Default.GetBytes(message);
+                    clientSocket.Send(buffer2);
+
+                    clientSocket.Receive(buffer);
+                    incommingMessage = Encoding.Default.GetString(buffer);
+                    incommingMessage = incommingMessage.Substring(0, incommingMessage.IndexOf("\0"));
+                    richTextBoxLog.AppendText("Server: " + incommingMessage + "\n");
+
+                    connected = false; // TERMINATE THE LOOP AFTER 1 CYCLE
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        richTextBoxLog.AppendText("The client is disconnected.");
+                    }
+                    clientSocket.Close();
+                    connected = false; // TERMINATE THE LOOP
+                }
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBoxIp.Text;
+            int portNumber;
+
+            if(Int32.TryParse(textBoxPort.Text, out portNumber))
+            {
+                //richTextBoxLog.AppendText(portNumber.ToString());
+                try
+                {
+                    clientSocket.Connect(IP, portNumber);
+                    connected = true;
+                    richTextBoxLog.AppendText("Connection established... \n");
+
+                    Thread recieveThread = new Thread(Recieve);
+                    recieveThread.Start();
+                }
+                catch
+                {
+                    richTextBoxLog.AppendText("Could not connect to the server. \n");
+                }
+            }
+            else
+            {
+                richTextBoxLog.AppendText("Problem occurred while connecting. \n");
+            }
+        }
+    }
+}
